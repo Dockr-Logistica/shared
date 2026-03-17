d5fa4e13b71a86d3b43cc73be03f7858d665e00b
Julio Lena <juliolena25@gmail.com>
2026-03-17 03:04:23 -0300

    [Cleanup] Remove legacy axios and react-query hooks

--- hooks/index.ts
+++ hooks/index.ts
@@ -1,25 +1,5 @@
-export { default as api } from './api'
-export { apiClient } from './client'
-export { queryClient } from './queryClient'
-
 export { useAPI } from './useAPI'
-export {
-  blockingAPI,
-  blockingGet,
-  blockingPost,
-  blockingPut,
-  blockingPatch,
-  blockingDelete
-} from './useBlockingAPI'
-
-export { useLogin } from './useLogin'
+export { blockingAPI } from './useBlockingAPI'
 export { useForgotPassword } from './useForgotPassword'
 export { useResetPassword } from './useResetPassword'
-
-export { useCalculateQuote } from './useCalculateQuote'
-
 export { useWizard } from './useWizard'
-
-export { default as authService } from './auth'
-
-export { default as quoteService } from './quote'
